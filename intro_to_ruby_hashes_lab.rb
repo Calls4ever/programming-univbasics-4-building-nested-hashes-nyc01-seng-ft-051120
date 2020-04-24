73a732962d9b0d9f3e0fd092eeda8df0128dcad6
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-24 17:53:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,5 +48,5 @@ def monopoly_with_fourth_tier
                                 }
                           }
                         }
-                        puts monopoly[:railroads][:names][:reading_railroad][:mortgage_value]
+                        
 end
